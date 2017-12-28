3ef6b264d82e8bee23842580eb525bf51a36ce85
Andy <andy@192.168.1.7>
2017-12-28 21:57:53 +0200

    One file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # crawler
+
+A simple algorithm that searches a HTML element on all the pages of a website.
--- crwl.py
+++ crwl.py
@@ -1,46 +1,52 @@
+from bs4 import BeautifulSoup
+from get_elements_from_html import getElementsFromHTML
+
 import urlparse
 import mechanize
-from bs4 import BeautifulSoup
 
-url = "http://www.filmeonline.biz/"
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-urls = [url]
-visited = [url]
+def getAllPagesFromWebsite(url):
+	urls = [url]
+	visited = [url]
+	added = []
 
-nodups = []
+	file = open('movies.txt', 'a')
 
-file = open('movies.txt', 'a')
+	while len(urls) > 0:
+		try:
+			print len(urls)
+			
+			br.open(urls[0])
+			urls.pop(0)
+			for link in br.links():
+				newurl =  urlparse.urljoin(link.base_url,link.url)
 
-j = 0
+				if newurl.endswith('.html') and newurl not in added:
+					visited.append(newurl)
+					added.append(newurl)
+					file.write(newurl + "\n")
+				else:
+					if newurl not in visited and url in newurl:
+						visited.append(newurl)
+						urls.append(newurl)
+		except:
+			print "error"
+			urls.pop(0)
 
-#while len(urls) > 0:
-while j < 1000:
-	currentUrl = br.open(urls[0])
-	urls.pop(0)
+def getElementsFromHTML(brws):
+	f = open("movies.txt","r")
 
-	for link in br.links():
-		newurl =  urlparse.urljoin(link.base_url,link.url)
+	for line in f:
+		soup = BeautifulSoup(brws.open(line), "html.parser")
+		iframe = soup.findAll('iframe')
 
-		if newurl.endswith('.html'):
-			newPage = br.open(newurl)
-			if newPage:
-				soup = BeautifulSoup(newPage, "html.parser")
-				iframe = soup.findAll('iframe')
+		if len(iframe) > 0:
+			print "Title: " + soup.title.string.encode('utf-8') + ' - ' + str(len(iframe))
 
-				if len(iframe) > 0 and newurl not in nodups:
-					file.write("Title: " + soup.title.string.encode('utf-8') + "\n\n")
-					file.write("START IFRAMES =====================\n")
-					for i in iframe:
-						file.write(str(i) + "\n")
-					file.write("END IFRAMES =======================\n\n\n")
-					nodups.append(newurl)
 
-		if newurl not in visited and url in newurl:
-			visited.append(newurl)
-			urls.append(newurl)
-	j += 1
-
-print "Movies added " + str(len(nodups))
+website = "http://www.filmeonline.biz/"
+getAllPagesFromWebsite(website)
+getElementsFromHTML(br)
